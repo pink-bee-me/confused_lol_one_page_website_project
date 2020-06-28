5ab96a4730e0203a0c055cd58bc148823337959a
PinkBeeMe <51229658+PinkBeeMe@users.noreply.github.com>
2020-06-27 20:14:01 -0500

    still working out the modal/lightbox issues on one-page website update

--- JavaScript/JavaScript_one-page_project.js
+++ JavaScript/JavaScript_one-page_project.js
@@ -22,3 +22,46 @@ lightbox.addEventListener("click", e =>{ //add an event listener for "click" and
                                             // we do not want to exit the light box unless we click in the black area of lightbox)
     lightbox.classList.remove("active") //removes the active class which will hide our lightbox again
 })
+
+// Modal number Two SetUp //
+//Open Modal
+function openModal() {
+    document.getElementById("myModal").style.display = "block";
+}
+
+//Close the Modal
+function closeModal() {
+    document.getElementById("myModal").style.display ="none"
+}
+
+var slideIndex=1;
+showSlides (slideIndex);
+
+//Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail Image Controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementById("demo");
+    var captionText = document.getElementById("caption");
+    if (n > slides.length){ slideIndex = 1 }
+    if (n < 1) { slidesIndex = slides.length}
+    for (i = 0; 1< slides.length; i++) {
+        slides[i]style.display="none";
+    }
+    for (i=0;i<dots.length;i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+    captionText.innerHTML = dots[slideIndex-1].alt;
+    }
+
